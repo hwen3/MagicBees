25e32711871676b9c852fcfa20cbb896c6f3a8a7
Elec332 <arnout1998@gmail.com>
2018-01-04 19:46:39 +0100

    Enables showAllBees by default, closes #49

--- src/main/java/magicbees/util/Config.java
+++ src/main/java/magicbees/util/Config.java
@@ -23,21 +23,21 @@ public class Config implements IConfiguration {
 
     public static boolean oldJarModel = false;
 
-    public static boolean showAllBees = false;
+    public static boolean showAllBees = true;
 
     public static boolean removeUnneededBees = true;
 
     @Override
     public void reload(Configuration config) {
-        magnetSound = config.getBoolean("magnetSound", Configuration.CATEGORY_GENERAL, true, "Enables/Disables the magnet sounds.");
+        magnetSound = config.getBoolean("magnetSound", Configuration.CATEGORY_GENERAL, magnetSound, "Enables/Disables the magnet sounds.");
         magnetMaxLevel = config.getInt("magnetMaxLevel", Configuration.CATEGORY_GENERAL, 9, 1, 16, "Sets max level of the Mysterious Magnet.");
         magnetBaseRange = config.getInt("magnetBaseRange", Configuration.CATEGORY_GENERAL, 3, 1, 8, "Sets base range of the Mysterious Magnet.");
         magnetLevelMultiplier = config.getFloat("magnetLevelMultiplier", Configuration.CATEGORY_GENERAL, 0.75f, 0.1f, 8,"Used to calculate the range of the Mysterious Magnet; Range = baseValue + level * multiplier");
-        moonDialShowsPhaseInText = config.getBoolean("moonDialShowsPhaseInText", Configuration.CATEGORY_GENERAL, true, "Whether the MoonDial should show the MoonPhase in it's tooltip.");
-        fancyJarRenderer = config.getBoolean("fancyJarRenderer", Configuration.CATEGORY_CLIENT, true, "Whether to render the bee inside the Bee Collector's Jar with a TESR.");
-        oldJarModel = config.getBoolean("oldJarmodel", Configuration.CATEGORY_CLIENT, false, "Set to true to use the old model for the Bee Collector's Jar");
-        showAllBees = config.getBoolean("showAllBees", Configuration.CATEGORY_GENERAL, false, "Set to true to show all MagicBees bees in the creative tab/JEI.");
-        removeUnneededBees = config.getBoolean("removeUnneededBees", Configuration.CATEGORY_GENERAL, true, "When true, removes all bees related to mods that are not present from the game.");
+        moonDialShowsPhaseInText = config.getBoolean("moonDialShowsPhaseInText", Configuration.CATEGORY_GENERAL, moonDialShowsPhaseInText, "Whether the MoonDial should show the MoonPhase in it's tooltip.");
+        fancyJarRenderer = config.getBoolean("fancyJarRenderer", Configuration.CATEGORY_CLIENT, fancyJarRenderer, "Whether to render the bee inside the Bee Collector's Jar with a TESR.");
+        oldJarModel = config.getBoolean("oldJarmodel", Configuration.CATEGORY_CLIENT, oldJarModel, "Set to true to use the old model for the Bee Collector's Jar");
+        showAllBees = config.getBoolean("showAllBees", Configuration.CATEGORY_GENERAL, showAllBees, "Set to true to show all MagicBees bees in the creative tab/JEI.");
+        removeUnneededBees = config.getBoolean("removeUnneededBees", Configuration.CATEGORY_GENERAL, removeUnneededBees, "When true, removes all bees related to mods that are not present from the game.");
     }
 
     //@Configurable.Class(comment = "Here you can configure certain bees")
